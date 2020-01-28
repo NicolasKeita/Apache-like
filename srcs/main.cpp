3ca850017e5363bffcfadb50b562b98134e857ae
Nicolas KEITA <nicolas.keita@epitech.eu>
2020-01-28 13:55:11 +0100

    Created main.cpp of the zia binary

---
+++ srcs/main.cpp
@@ -0,0 +1,11 @@
+/*
+** Creation Year : 2020 ; Month : 01 ; Day : 28.
+*/
+
+#include "MyProgArgs.hpp"
+
+int main(int argc, char *argv[], char **env)
+{
+    uti::MyProgArgs args(argc, argv, env, 0);
+    return 0;
+}
